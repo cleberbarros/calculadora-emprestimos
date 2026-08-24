f2b8e8f9ada8447e56cb2c346a719368489bfe94
Cléber Barros <cleberbarros.ti@gmail.com>
2026-08-24 20:11:19 +0100

    adiciona validações das datas do empréstimo

--- src/main/java/br/com/totvs/desafio/calculadoraemprestimos/service/EmprestimoService.java
+++ src/main/java/br/com/totvs/desafio/calculadoraemprestimos/service/EmprestimoService.java
@@ -8,7 +8,21 @@ import org.springframework.stereotype.Service;
 public class EmprestimoService {
 
     public CalculoEmprestimoResponse calcular(CalculoEmprestimoRequest request) {
+        validarDatas(request);
+
         // TODO: Implementar lógica financeira de cálculo do empréstimo
         throw new UnsupportedOperationException("Cálculo de empréstimo ainda não implementado");
+    }
+
+    private void validarDatas(CalculoEmprestimoRequest request) {
+        if (!request.getDataFinal().isAfter(request.getDataInicial())) {
+            throw new IllegalArgumentException("A data final deve ser posterior à data inicial.");
+        }
+
+        if (!request.getPrimeiroPagamento().isAfter(request.getDataInicial())
+                || !request.getPrimeiroPagamento().isBefore(request.getDataFinal())) {
+            throw new IllegalArgumentException(
+                    "A data do primeiro pagamento deve ser posterior à data inicial e anterior à data final.");
+        }
     }
 }
