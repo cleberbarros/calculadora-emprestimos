82ad6d2345a50a1d72e30f17d73ed72d57165cd1
Cléber Barros <cleberbarros.ti@gmail.com>
2026-08-24 22:02:16 +0100

    adiciona geração das datas e quantidade de parcelas

--- src/main/java/br/com/totvs/desafio/calculadoraemprestimos/service/EmprestimoService.java
+++ src/main/java/br/com/totvs/desafio/calculadoraemprestimos/service/EmprestimoService.java
@@ -4,11 +4,22 @@ import br.com.totvs.desafio.calculadoraemprestimos.dto.CalculoEmprestimoRequest;
 import br.com.totvs.desafio.calculadoraemprestimos.dto.CalculoEmprestimoResponse;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 @Service
 public class EmprestimoService {
 
     public CalculoEmprestimoResponse calcular(CalculoEmprestimoRequest request) {
         validarDatas(request);
+
+        List<LocalDate> datasCalculo = gerarDatasCalculo(request);
+        int quantidadeParcelas = calcularQuantidadeParcelas(request);
 
         // TODO: Implementar lógica financeira de cálculo do empréstimo
         throw new UnsupportedOperationException("Cálculo de empréstimo ainda não implementado");
@@ -24,5 +35,61 @@ public class EmprestimoService {
             throw new IllegalArgumentException(
                     "A data do primeiro pagamento deve ser posterior à data inicial e anterior à data final.");
         }
+    }
+
+    private List<LocalDate> gerarDatasCalculo(CalculoEmprestimoRequest request) {
+        Set<LocalDate> datas = new TreeSet<>();
+
+        LocalDate dataInicial = request.getDataInicial();
+        LocalDate dataFinal = request.getDataFinal();
+        LocalDate primeiroPagamento = request.getPrimeiroPagamento();
+
+        datas.add(dataInicial);
+
+        LocalDate data = dataInicial.with(TemporalAdjusters.lastDayOfMonth());
+
+        while (data.isBefore(dataFinal)) {
+            if (data.isAfter(dataInicial)) {
+                datas.add(data);
+            }
+
+            data = data.plusMonths(1)
+                    .with(TemporalAdjusters.lastDayOfMonth());
+        }
+
+        int diaPagamento = primeiroPagamento.getDayOfMonth();
+        LocalDate pagamento = primeiroPagamento;
+
+        while (pagamento.isBefore(dataFinal)) {
+            datas.add(pagamento);
+
+            YearMonth mes = YearMonth.from(pagamento).plusMonths(1);
+            int dia = Math.min(diaPagamento, mes.lengthOfMonth());
+
+            pagamento = mes.atDay(dia);
+        }
+
+        datas.add(dataFinal);
+
+        return new ArrayList<>(datas);
+    }
+
+    private int calcularQuantidadeParcelas(CalculoEmprestimoRequest request) {
+        LocalDate primeiroPagamento = request.getPrimeiroPagamento();
+        LocalDate dataFinal = request.getDataFinal();
+
+        int quantidade = 0;
+        int diaPagamento = primeiroPagamento.getDayOfMonth();
+        LocalDate pagamento = primeiroPagamento;
+
+        while (pagamento.isBefore(dataFinal)) {
+            quantidade++;
+
+            YearMonth mes = YearMonth.from(pagamento).plusMonths(1);
+            int dia = Math.min(diaPagamento, mes.lengthOfMonth());
+
+            pagamento = mes.atDay(dia);
+        }
+        return quantidade + 1;
     }
 }
