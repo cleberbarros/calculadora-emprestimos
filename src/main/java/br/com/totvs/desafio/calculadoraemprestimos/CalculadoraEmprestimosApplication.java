91eb727e9fcc8ff8f999e6099cb4d5f4267540ba
Cléber Barros <cleberbarros.ti@gmail.com>
2026-08-24 19:28:50 +0100

    inicialização do projeto

---
+++ src/main/java/br/com/totvs/desafio/calculadoraemprestimos/CalculadoraEmprestimosApplication.java
@@ -0,0 +1,13 @@
+package br.com.totvs.desafio.calculadoraemprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraEmprestimosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraEmprestimosApplication.class, args);
+    }
+
+}
